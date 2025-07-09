64d535e08b9219c147b061099a894b54924127f9
mohammed <mohammedsheikhalard@gmail.com>
2025-07-09 10:08:08 +0300

    This is the plan Implementation

--- Home_Wrokout/app/Http/Controllers/PlanController.php
+++ Home_Wrokout/app/Http/Controllers/PlanController.php
@@ -5,5 +5,100 @@ namespace App\Http\Controllers;
 use App\Models\Plan;
 use App\Http\Requests\StorePlanRequest;
 use App\Http\Requests\UpdatePlanRequest;
+use App\Http\Resources\PlanResource;
+use App\Models\Admin;
+use App\Traits\apiResponseTrait;
+use Illuminate\Http\Request;
 
-class PlanController extends Controller {}
+class PlanController extends Controller
+{
+
+    use apiResponseTrait;
+
+    public function addNewPlan(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'number_of_day_to_train' => 'required',
+            'level_id' => 'required'
+        ]);
+
+        $admin = $request->user();
+
+        $plan  = Plan::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'number_of_day_to_train' => $request->number_of_day_to_train,
+            'admin_id' => $admin->id,
+            'level_id' => $request->level_id,
+        ]);
+
+        if ($plan == null) {
+            return $this->apiResponse($plan, "Something Went Wrong When adding New plan", 404);
+        }
+
+        return $this->apiResponse($plan, "Your New Plan Added Successfully", 200);
+    }
+
+    public function updatePlan(Request $request)
+    {
+        $plan = Plan::find($request->id);
+
+        if ($plan == null) {
+            return $this->apiResponse(null, "Your Plan Was not found", 404);
+        }
+
+        $plan->update($request->all());
+
+        return $this->apiResponse($plan, "plan Updated Successfully", 200);
+    }
+
+    public function getPlan(Request $request)
+    {
+        $plan = Plan::find($request->id);
+
+        if ($plan == null) {
+            return $this->apiResponse(null, "Your Plan Was not found", 404);
+        }
+
+        return $this->apiResponse($plan, "This is Your Plan", 200);
+    }
+
+    public function deletePlan(Request $request)
+    {
+        $plan = Plan::find($request->id);
+
+        if ($plan == null) {
+            return $this->apiResponse(null, "Your Plan Was not found", 404);
+        }
+
+        $plan->delete();
+
+        return $this->apiResponse(null, "Your Plan Deleted Successfully", 200);
+    }
+
+    public function restorePlan(Request $request)
+    {
+        $plan = Plan::withTrashed()->find($request->id);
+
+        if ($plan == null) {
+            return $this->apiResponse(null, "Your Plan Was not found", 404);
+        }
+
+        $plan->restore();
+
+        return $this->apiResponse($plan, "Your Plan Restored Successfully", 200);
+    }
+
+    public function getAllPlans()
+    {
+        $plans = Plan::all();
+
+        if ($plans->isEmpty()) {
+            return $this->apiResponse(null, "No Plans Found", 404);
+        }
+
+        return $this->apiResponse(PlanResource::collection($plans), "This is all plans", 200);
+    }
+}
