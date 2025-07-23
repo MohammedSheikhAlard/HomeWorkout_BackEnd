f7e02a826a88bbae1357c7cd15483e66c4c33858
mohammed <mohammedsheikhalard@gmail.com>
2025-07-23 20:21:40 +0300

    This is plan Day Exercises Implementation

--- Home_Wrokout/app/Http/Controllers/PlanDayExerciseController.php
+++ Home_Wrokout/app/Http/Controllers/PlanDayExerciseController.php
@@ -2,8 +2,122 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PlanDay;
+use Illuminate\Http\Request;
+use App\Models\ExerciseLevel;
 use App\Models\PlanDayExercise;
+use App\Traits\apiResponseTrait;
+use PhpParser\Node\Stmt\Return_;
+use Illuminate\Support\Facades\DB;
 use App\Http\Requests\StorePlanDayExerciseRequest;
 use App\Http\Requests\UpdatePlanDayExerciseRequest;
+use Illuminate\Auth\Events\Validated;
 
-class PlanDayExerciseController extends Controller {}
+class PlanDayExerciseController extends Controller
+{
+
+    use apiResponseTrait;
+
+    public function addNewPlanDayExercise(Request $request)
+    {
+        $request->validate([
+            'plan_day_id' => 'required',
+            'exercies_level_id' => 'required|unique:plan_day_exercises',
+            'exercies_order' => 'required|unique:plan_day_exercises'
+        ]);
+
+        $planDay = PlanDay::find($request->plan_day_id);
+
+        if ($planDay->is_rest_day) {
+            return $this->apiResponse(null, "You can't add a exercieses for a rest day", 200);
+        }
+
+        $planDayExercise = PlanDayExercise::create([
+            'plan_day_id' => $request->plan_day_id,
+            'exercies_level_id' => $request->exercies_level_id,
+            'exercies_order' => $request->exercies_order
+        ]);
+
+        if ($planDayExercise == null) {
+            return $this->apiResponse(null, "Your plan Day Exercise added falied", 500);
+        }
+
+        $exercies = ExerciseLevel::find($request->exercies_level_id);
+        $planDay = PlanDay::find($request->plan_day_id);
+
+        $planDay->total_calories += $exercies->calories;
+
+        $planDay->save();
+
+        return $this->apiResponse($planDayExercise, "New Exercise Added For That Day Plan", 200);
+    }
+
+    public function updatePlanDayExercise(Request $request)
+    {
+        $planDayExercise = PlanDayExercise::find($request->id);
+
+        if ($planDayExercise->exercies_level_id != $request->exercies_level_id) {
+            $request->validate([
+                'id' => 'required',
+                'exercies_level_id' => 'required',
+                'exercies_order' => 'required|unique:plan_day_exercises'
+            ]);
+        }
+
+        if ($planDayExercise == null) {
+            return $this->apiResponse(null, "Plan Day Exercise not Found", 404);
+        }
+
+
+        if ($request->exercies_level_id != $planDayExercise->exercies_level_id) {
+            $planDay = PlanDay::find($planDayExercise->plan_day_id);
+
+            $exercise = ExerciseLevel::find($request->exercies_level_id);
+            $preExercise = ExerciseLevel::find($planDayExercise->exercies_level_id);
+
+            $planDay->total_calories -= $preExercise->calories;
+            $planDay->total_calories += $exercise->calories;
+
+            $planDay->save();
+        }
+
+
+
+        $planDayExercise->update($request->all());
+
+        return $this->apiResponse($planDayExercise, "Plan Day Exercise Updated Successfully", 200);
+    }
+
+    public function getPlanDayExercise(Request $request)
+    {
+        $planDayExercise = PlanDayExercise::find($request->id);
+
+        if ($planDayExercise == null) {
+            return $this->apiResponse(null, "Plan Day Exercise not found", 404);
+        }
+
+        return $this->apiResponse($planDayExercise, "This is Your Plan Day Exercise", 200);
+    }
+
+    public function getAllPlanDayExercises(Request $request)
+    {
+
+
+        $planDayExercises = DB::table('plan_day_exercises')
+            ->join('plan_days', 'plan_days.id', '=', 'plan_day_exercises.plan_day_id', 'inner')
+
+            ->join('exercise_levels', 'exercise_levels.id', '=', 'plan_day_exercises.exercies_level_id', 'inner')
+
+            ->join('exercises', 'exercises.id', '=', 'exercise_levels.exercise_id', 'inner')
+
+            ->select('plan_day_exercises.id', 'exercises.name', 'exercises.description', 'exercises.image_path', 'exercise_levels.calories', 'exercise_levels.number_of_rips', 'plan_day_exercises.exercies_order')
+
+            ->where('plan_day_exercises.plan_day_id', '=', $request->plan_day_id)->get();
+
+        if ($planDayExercises == null) {
+            return  $this->apiResponse(null, "something went wrong", 400);
+        }
+
+        return $this->apiResponse($planDayExercises, "this is your Exercises for today", 200);
+    }
+}
